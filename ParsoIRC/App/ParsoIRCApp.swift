e9874a71c4eaa9dcb2878a18131905a2d113106c
John Arley Burns <jburns@idslabs.org>
2026-04-17 11:28:32 -0400

    Fix: restore working App with body: some Scene

--- ParsoIRC/App/ParsoIRCApp.swift
+++ ParsoIRC/App/ParsoIRCApp.swift
@@ -2,9 +2,28 @@ import SwiftUI
 
 @main
 struct ParsoIRCApp: App {
-    var body: some View {
-        Text("Test")
+    @StateObject private var ircManager = IRCClientManager.shared
+    @StateObject private var appState = AppState.shared
+    @StateObject private var debugLog = DebugLogManager.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            SimpleConnectView()
+                .environmentObject(ircManager)
+                .environmentObject(appState)
+                .environmentObject(debugLog)
+        }
     }
+}
+
+@MainActor
+class AppState: ObservableObject {
+    static let shared = AppState()
+    
+    @Published var currentUser: User?
+    @Published var isAuthenticated = false
+    
+    init() {}
 }
 
 @MainActor
